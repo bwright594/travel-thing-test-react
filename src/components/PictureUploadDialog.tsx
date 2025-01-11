276a5b284516f3d24113297cdf022de58e92aa6a
Bennett Wright <bwright594@gmail.com>
2025-01-11 00:32:16 -0600

    fix upload another day and validation

--- src/components/PictureUploadDialog.tsx
+++ src/components/PictureUploadDialog.tsx
@@ -1,5 +1,5 @@
-import { Backdrop, Box, Button, CircularProgress, Dialog, DialogTitle, TextField, Typography } from '@mui/material';
-import { useState } from 'react';
+import { Backdrop, Box, Button, CircularProgress, Dialog, DialogActions, DialogTitle, TextField, Tooltip, Typography } from '@mui/material';
+import { useEffect, useMemo, useState } from 'react';
 import { defaultPicture, Picture, useAddPictureMutation } from '../api-slice';
 
 export interface PictureUploadDialogProps {
@@ -10,11 +10,10 @@ export interface PictureUploadDialogProps {
 }
 
 export default function PictureUploadDialog({ open, onClose, person, date }: PictureUploadDialogProps) {
-  const currentDefaultPicture = { ...defaultPicture, person, date };
-  const [ picture, setPicture ] = useState<Picture>(currentDefaultPicture);
+  const [ picture, setPicture ] = useState<Picture>({ ...defaultPicture, person, date });
   const [ addPicture, { data, isLoading } ] = useAddPictureMutation();
+
   const handleSubmit = () => {
-    console.log(picture);
     addPicture(picture)
       .then(() => console.log(data))
       .catch((e) => console.error(e))
@@ -23,7 +22,7 @@ export default function PictureUploadDialog({ open, onClose, person, date }: Pic
 
   const handleClose = () => {
     onClose();
-    setPicture(currentDefaultPicture);
+    setPicture({ ...defaultPicture, person, date });
   }
 
   const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,20 +39,42 @@ export default function PictureUploadDialog({ open, onClose, person, date }: Pic
     }
   }
 
+  const uploadButtonTooltip = useMemo(() => {
+    console.log(picture);
+    let tooltip = '';
+    if (!picture.name && picture.url) {
+      tooltip = 'You must select a picture name';
+    } else if (picture.name && !picture.url) {
+      tooltip = 'You must select a picture';
+    } else if (!picture.name && !picture.url) {
+      tooltip = 'You must select a picture name and a picture';
+    }
+    console.log('tooltip', tooltip);
+    return tooltip;
+  }, [picture.name, picture.url]);
+
+  useEffect(() => {
+    setPicture(previous => ({ ...previous, date}));
+  }, [date]);
+
   return (
     <Dialog onClose={() => {}} open={open}>
       <DialogTitle>Upload Picture</DialogTitle>
       <TextField label='Name' onChange={(e) => setPicture((previous) => ({ ...previous, name: e.target.value }))}/>
       <input type='file' accept='image/*' onChange={handleUpload}/>
-      <Box>
+      <DialogActions>
         {picture?.url && <Box><Typography> Preview </Typography><img width='100%' height='100%' src={picture.url} /></Box> }
         <Button onClick={handleClose} variant='contained'>
           Cancel
         </Button>
-        <Button variant='contained' onClick={handleSubmit}>
-          Upload
-        </Button>
-      </Box>
+        <Tooltip title={uploadButtonTooltip} placement='top' disableInteractive>
+          <span>
+            <Button disabled={uploadButtonTooltip !== ''} variant='contained' onClick={handleSubmit}>
+              Upload
+            </Button>
+          </span>
+        </Tooltip>
+      </DialogActions>
       <Backdrop open={isLoading}>
         <CircularProgress/>
       </Backdrop>
